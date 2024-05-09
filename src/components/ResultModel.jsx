c45e761b6b2109746d0f37488c190671e3510993
gaurav01dec <tech.gauravverma@gmail.com>
2024-05-09 19:42:50 +0530

    second commit

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,14 +1,15 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
 
-const ResultNodel= forwardRef(function ResultModel({result, targettime ,remainingTime, onReset} ,ref) {
+const ResultNodel = forwardRef(function ResultModel({ result, targettime, remainingTime, onReset }, ref) {
     const dialog = useRef();
 
-    const userLost = remainingTime<=0;
-    const formattedRemainingTime = (remainingTime/1000).toFixed(2)
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+const score = Math.round((1-remainingTime/(targettime*1000))*100)
 
-    useImperativeHandle(ref,()=>{
-        return{
-            open(){
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
                 dialog.current.showModal()
             }
         }
@@ -16,13 +17,14 @@ const ResultNodel= forwardRef(function ResultModel({result, targettime ,remainin
     return (
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score : {score}</h2>}
             <p>The target time was <strong>{targettime} seconds.</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
-    )                                                       
+    )
 })
 
 export default ResultNodel;
